0eb84bb7f86c75d4d2717eeaf0aed428787452da
Owain <owain.williamspopping123@gmail.com>
2019-12-13 21:47:12 +0000

    Working Copy of Script, some tables copied

--- excel_to_json_converter.py
+++ excel_to_json_converter.py
@@ -11,25 +11,44 @@ def begin_entry():
         print("="*50)
         year_range = input("Year Range\n")
         print("="*50)
-        table_data = input("Table Data\n")
-        table_data_formatted = format_data(table_data)
+        print("Table Data\n")
+        lines = []
+        while True:
+            line = input()
+            if line:
+                lines.append(line)
+            else:
+                break
+        text = '\n'.join(lines)
+        table_data_formatted = format_data_insert(text, table_name)
         table_year_range_formatted = format_year_range(year_range)
-        query = f"""CREATE TABLE {table_name} (
+
+        create_table_query = f"""CREATE TABLE '{table_name}' (
 \t'row_name' varchar(255),
 {table_year_range_formatted}
 );"""
         print("="*50)
-        print(query)
+        print(create_table_query)
+        print("="*50)
+        print(table_data_formatted[0])
+        print("="*50)
+        print(table_data_formatted[len(table_data_formatted)-1])
+        print("="*50)
+
         while True:
             ans = input("Submit(y/n)\n")
             if ans == "y":
-                cur.execute(query)
+                cur.execute(create_table_query)
+                for query in table_data_formatted:
+                    cur.execute(query)
+                conn.commit()
                 break
             elif ans == "n":
                 break
             else:
                 print("="*50)
                 print("Please enter only y/n")
+                print(ans)
 
 def create_connection(db_file):
     conn = None
@@ -43,11 +62,18 @@ def create_connection(db_file):
 
 def parse_input(string):
     split_text = string.split()
-    output = split_text
-    return output
+    return split_text
 
-format_data(data):
-    data.split("\n")
+def format_data_insert(data, table):
+    each_line = data.split("\n")
+    queries = []
+    for line in each_line:
+        line = line.split("\t")
+        title = line[0]
+        values = "', '".join(line[1:])
+        query = f"INSERT INTO '{table}' VALUES ('{title}', '{values}');"
+        queries.append(query)
+    return queries
 
 def format_year_range(years):
     years_list = parse_input(years)
@@ -60,7 +86,7 @@ def format_year_range(years):
         else:
             query_part += f"\t'{year}' float"
     return query_part
-        
+
 
 if __name__ == "__main__":
     begin_entry()
