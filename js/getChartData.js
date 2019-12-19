e472f0c1a88f70c3e18f069c169994174425f90d
Owain Williams <owain.williams.213@gmail.com>
2019-12-19 16:23:06 +0000

    Beginning to convert database to txt file to be read by js

---
+++ js/getChartData.js
@@ -0,0 +1,49 @@
+var tableName = "Carbon dioxide (CO2) emissions by industry group, 1990 to 2017 (mtCO2/a)"
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('fulldata.db');
+db.serialize(function() {
+  db.each(`SELECT * FROM ${tableName}`, function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
