f39653d32d5024a9476b10a7deed5bb56f41a2f5
Owain Williams <owain.williams.213@gmail.com>
2019-12-20 14:41:56 +0000

    Adding more notes to pages

--- resources/graph_notes.js
+++ resources/graph_notes.js
@@ -15,13 +15,61 @@ var graph_notes = [
   ],
   [
     "General negative time correlation with no major outliers.",
-    "~27% decrease of total CO2 emissions from 1990 to 2017."
-  ],
-  [
+    "~27% decrease of total CO2 emissions from 1990 to 2017.",
     "CO2 emissions were the main player in the total greenhouse gas distribution, meaning the follow similar patterns."
   ],
   [
     "CO2 emissions were the main player in the total greenhouse gas distribution, meaning the follow similar patterns."
   ],
-
+  [
+    "General negative time correlation with no major outliers.",
+    "'Agriculture, forestry and fishing' remains practically the same from 1990 to 2017."
+  ],
+  [
+    "General negative time correlation.",
+    "Most of the food industries have increased their methane emissions, such as 'Manufacture of dairy products' and 'Manufacture of other food products.'",
+    "There is an overwhelming decrease of 97.8% in the methane emission of the mining of coal and ignite."
+  ],
+  [
+    "General negative time correlation.",
+    "There is a 98% decrease in manufacturing N2O emissions."
+  ],
+  [
+    "Total N2O emissions decrease pretty heavily from 45-19 MtN2O-CO2e/a from 1990 to 2017 with a signifigant drop in 1998 to 1999 of 10 MtN2O-CO2e/a",
+    "'Consumer expenditure - travel' decreases from 1200-460 mtN2O-CO2e/a from 1995 to 2017.",
+    "'Manufacturing of petrochemicals' drops from 20000-10 mtN2O-CO2e/a from 1990 to 2017.",
+    "'Manufacturing of industrial gases' increases from 50-200 mtN2O-CO2e/a from 1990 to 2017.",
+    "'Other nitrogen compounds' drops from 4000-40 mtN2O-CO2e/a from 1990 to 2017."
+  ],
+  [
+    "HFC emissions remain at the same level of about 14 MtHCF-CO2e/a.",
+    "The use of HCF only seems to be discovered in 1993 where there is a large jump in other industries' use.",
+    "Manufacturing was a massive emitter of HCF, meaning the increase of HFC from other industries in 1993 was neglegable.",
+    "There is a drop in Manufacturing emissions from 14-1.7 MtHCF-CO2e/a from 1990 to 2017.",
+    "Dispite manufacturing's drop in emissions, the other industries make up for the decrease by increasing their own emissions drastically."
+  ],
+  [
+    "In this graph we can see that the main Manufacturing player was the manufacture of petrochemicals, similar to N2O emissions."
+  ],
+  [
+    "Total PFC emissions decrease from 1700-370 mtPFC-CO2e/a from 1990 to 2017.",
+    "Manufacturing is the largest contributor towards PFC emissions as the lines for 'total PFC emission' and 'manufacturing' are almost identical.",
+    "Most of the non-zero lines seem to peak in 2001 before decreasing to zero. What is interesting about this is that the rate of decrease seems to be almost exactly equal to the rate of increase.",
+    "Oddly, 'water supply' drops to zero in 1997 and doesnt become used again until 2011, only to zero out again in 2014."
+  ],
+  [
+    "This graph shows us that the manufacturing PFC emissions are comprised of petrochemicals, computer parts, aluminium production.",
+    "The PFC emissions for 'computer, electronic, communication and optical products' in 2017 is more than twice what it was in 1990."
+  ],
+  [
+    "The only source of NF3 emissions is Manufacturing.",
+    "NF3 emissions increase from 400-1700 tNF3-CO2e/a from 1990-2000.",
+    "NF3 emissions return back to a similar point from 1700-500 tNF3-CO2e/a from 2000-2017."
+  ],
+  [
+    "The only manufacturing NF3 emissions are from computer components."
+  ],
+  [
+    
+  ]
 ]
